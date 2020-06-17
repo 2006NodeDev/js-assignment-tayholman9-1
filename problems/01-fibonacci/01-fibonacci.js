85a55b635aa2f647c444ddcd7e9aabdbd79caefa
Taylor Holman <taylorholman@Taylors-MBP.home>
2020-06-17 15:07:22 -0400

    added fib sequence git push

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,18 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
-
+let memo ={
+ 1:0,
+ 2:1
 }
+
+let fib = (n) => {
+ if (memo[n]|| memo[n] ===0){
+  return memo[n]
+ }
+ else{
+  memo[n] = fib(n - 1) + fib(n - 2)
+  return memo[n]
+ }
+  }
+console.log(fib(15));
