8bbd945ab5dee470c7017bf83beb60976d9dc738
Taylor Holman <taylorholman@Taylors-MBP.home>
2020-06-18 14:10:10 -0400

    added reverse string

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,8 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
-}
+
+ return someStr.split('').reverse().join('')
+ }
+
+console.log(reverseStr('watermelon'));
