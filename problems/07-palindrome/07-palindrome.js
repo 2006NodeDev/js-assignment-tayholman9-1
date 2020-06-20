38d4e18d9d4f7f9cab3b861f45ee0f63806f8809
Taylor Holman <taylorholman@Taylors-MBP.home>
2020-06-20 18:16:15 -0400

    added objext creation

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,9 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
+
+const str = "racecar";
+
 function isPalindrome(someStr) {
-  
-}
+
+
--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,31 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+//object literal
+let personOL = {name: "Taylor", Age:23};
+console.log(personOL);
+
+//constructor
+let personConstructor = new Object();
+personConstructor.name = "Taylor";
+personConstructor.age = 23;
+console.log(personConstructor);
+
+
+//es6
+class es6Person{
+ constructor(name, age){
+ this.name=name;
+ this.age=age;
+}
+
+ }
+let person = new es6Person('Taylor', 23)
+console.log(person);
+
+
+
+
+
+
