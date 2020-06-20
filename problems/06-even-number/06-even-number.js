491894ae3cf575e35653dfc12c82d175ec543e47
Taylor Holman <taylorholman@Taylors-MBP.home>
2020-06-20 16:38:13 -0400

    added even number

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,16 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+if((someNum &1) == 0){
+ return true
 }
+return false;}
+
+console.log(isEven(4));
+console.log(isEven(-4));
+console.log(isEven(17));
+console.log(isEven(2345));
+console.log(isEven(989482));
+
+
+
