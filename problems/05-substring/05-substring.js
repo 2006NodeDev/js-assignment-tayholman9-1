f6b8746e0a2bf900b4a975b4b9892c6c7273692c
Taylor Holman <taylorholman@Taylors-MBP.home>
2020-06-22 15:46:53 -0400

    added object properties

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -5,4 +5,20 @@ If incorrect input is entered, throw an exception stating the input was incorrec
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
 
+    //get data type and return string
+  if(typeof(someStr) === 'string' && typeof(startIndex) === 'number' && Number.isInteger(endIndex))
+
+    //makes sure input falls within the index of the string
+      if (startIndex <= someStr.length && endIndex <= someStr.length && startIndex >= 0 && startIndex <= endIndex)
+          return someStr.substring(startIndex, endIndex)
+      throw new Error("Input Out Of Bounds Error")
 }
+
+
+console.log(substring('watermelon',3, 8));
+
+console.log(substring('watermelon', 0, 4));
+
+//output error out of bounds
+/*console.log(substring('watermelon',0,11));*/
+
--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,21 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
+ //for..in executes once for each property
+ for(variable in someObj){
+  console.log(`${variable}: ${someObj[variable]}`)
+}
+}
+
+function wine(nameOfWine, Varietal,color, countryOfOrigin,alcoholPercentage){
+  this.nameOfWine = nameOfWine
+  this.Varietal= Varietal
+  this.color = color
+  this.countryOfOrigin = countryOfOrigin
+  this.alcoholPercentage = alcoholPercentage
 
 }
+
+let Sterling = new wine("Sterling", "Meritage", "red", "United States", 13)
+
+objectProperties(Sterling)
