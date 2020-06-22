60d3f275c8a285aea268eb7dc2fad6581244d5f7
Taylor Holman <taylorholman@Taylors-MBP.home>
2020-06-22 13:02:20 -0400

    added palindrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,8 +2,16 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 
-const str = "racecar";
-
 function isPalindrome(someStr) {
+ let reverseStr = someStr.split("").reverse().join("")
+ if(someStr==reverseStr){
+  return true;
+ }else{
+  return false;
+ }
+}
+console.log(isPalindrome('racecar'))
+console.log(isPalindrome('palindrome'));
+console.log(isPalindrome('anna'));
 
 
